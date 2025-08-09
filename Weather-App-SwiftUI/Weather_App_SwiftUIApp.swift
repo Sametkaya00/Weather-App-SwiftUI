af7d95050261c7e9a131a8339136e3663dce90f0
Samet kaya <sametkaya.dev@gmail.com>
2025-08-09 15:55:48 +0300

    Initial Commit

---
+++ Weather-App-SwiftUI/Weather_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUIApp.swift
+//  Weather-App-SwiftUI
+//
+//  Created by samet kaya on 9.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
